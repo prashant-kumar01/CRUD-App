0f1766956c69a305c267de30052824bd90d786d0
prashant-kumar01 <prashantkumar.cse19@chitkarauniversity.edu.in>
2022-02-02 14:20:23 +0530

    Updates

---
+++ precache-manifest.7eac1eaa5a8d8d6d4a66de4af085fa7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05389478579dc22d8ad2aa666543e2a",
+    "url": "/CRUD-App/index.html"
+  },
+  {
+    "revision": "c2fd04345a55a3c8f9c8",
+    "url": "/CRUD-App/static/css/main.3009f149.chunk.css"
+  },
+  {
+    "revision": "1f5d6e286b76f5964aa9",
+    "url": "/CRUD-App/static/js/2.99d09b65.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/CRUD-App/static/js/2.99d09b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fd04345a55a3c8f9c8",
+    "url": "/CRUD-App/static/js/main.b1a16744.chunk.js"
+  },
+  {
+    "revision": "75ee93c954d0c9eb0362",
+    "url": "/CRUD-App/static/js/runtime-main.a1a0cb7f.js"
+  }
+]);
